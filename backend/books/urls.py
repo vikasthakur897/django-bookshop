80f39010171a811bb5926699cd4ba8b3a29e2cd8
VikasThakur <vikasthakur1723@gmail.com>
2025-11-03 19:47:18 +0530

    Create a simple django api for books

---
+++ backend/books/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
